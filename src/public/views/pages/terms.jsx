898d821ec595064c21fdc8eb7fa2138e37c666ca
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-15 22:05:12 -0500

    Wording, smaller terms

--- src/public/views/pages/terms.jsx
+++ src/public/views/pages/terms.jsx
@@ -10,7 +10,7 @@ export default class Terms extends React.Component {
                 <Header title='Terms' />
                 <div className='terms'>
                     <p>1. By using this website, you agree that you are currently a student at the Milwaukee School of Engineering.</p>
-                    <p>2. The funds and tickets credited to a student on the website are in my custodial possession, but they remain the property of the student.</p>
+                    <p>2. The funds and tickets credited to a student on the website are in my custodial possession, but they remain the property of that student.</p>
                     <p>3. I will make a reasonable effort to return all tickets to their owners before May 27, 2017. Any remaining tickets will be assumed abandoned and forfeit by the student.</p>
                     <p>4. I will make a reasonable effort to return all funds to their owners before June 27, 2017. Any remaining funds will be assumed abandoned and forfeit by the student.</p>
                     <p>5. I reserve the right to buy and sell tickets on this website, but I may only trade with my own funds and tickets.</p>
