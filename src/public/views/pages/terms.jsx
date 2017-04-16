979354c2ed5fc7489a580f56659f210d7333f0ae
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-15 19:59:07 -0500

    First draft of terms

--- src/public/views/pages/terms.jsx
+++ src/public/views/pages/terms.jsx
@@ -9,9 +9,11 @@ export default class Terms extends React.Component {
             <div id='terms'>
                 <Header title='Terms' />
                 <div className='terms'>
-                    <p>1. TODO</p>
-                    <p>2. TODO</p>
-                    <p>3. TODO</p>
+                    <p>1. By using this website, you agree that you are currently a student at the Milwaukee School of Engineering.</p>
+                    <p>2. The funds and tickets credited to a student on the website are in my custodial possession, but they remain the property of the student.</p>
+                    <p>3. I will make a reasonable effort to return all tickets to their owners before May 27, 2017. Any remaining tickets will be assumed abandoned and forfeit by the student.</p>
+                    <p>4. I will make a reasonable effort to return all funds to their owners before June 27, 2017. Any remaining funds will be assumed abandoned and forfeit by the student.</p>
+                    <p>5. I reserve the right to buy and sell tickets on this website, but I may only trade with my own funds and tickets.</p>
                 </div>
                 <Footer />
             </div>
