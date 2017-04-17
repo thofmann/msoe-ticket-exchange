bbea0b54e11258be09699f5431986ce69a85cf51
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-17 17:15:26 -0500

    Hash to hex string

--- src/lib/crypto.js
+++ src/lib/crypto.js
@@ -8,5 +8,5 @@ export function saltAndHash(password, salt) {
     password = Buffer.from(password, 'hex');
     salt = Buffer.from(password, 'hex');
     let concatenated = Buffer.concat([password, salt]);
-    return crypto.createHash('sha256').update(concatenated).digest();
+    return crypto.createHash('sha256').update(concatenated).digest('hex');
 }
