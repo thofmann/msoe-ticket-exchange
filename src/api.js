ff62233e0b49a0d623f95a005f08881d7f19162b
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-17 01:45:57 -0500

    Begin REST api

---
+++ src/api.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import student from './api/model';
+
+let app = express();
+
+app.use((req, res, next) => {
+    res.successJson = data => {
+        let response = {
+            success: true
+        };
+        if (typeof data === 'object') {
+            response.data = data;
+        }
+        res.json(response);
+    };
+    res.failureJson = message => {
+        let response = {
+            success: false
+        };
+        if (typeof message === 'string') {
+            response.message = message;
+        }
+        res.json(response);
+    }
+});
+
+app.use('/student', student);
+
+export default app;
---
+++ src/api/student.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+let app = express();
+
+app.post('/register', (req, res) => {
+    // TODO
+    res.successJson();
+});
