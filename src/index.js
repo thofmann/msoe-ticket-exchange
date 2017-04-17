3fa68c534813a380d35e0ce4ace3a8b044d64afc
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-17 16:51:30 -0500

    Have server use API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import path from 'path';
 import express from 'express';
+import bodyParser from 'body-parser';
+import api from './api';
 
 let app = express();
 
 app.use(express.static(path.join(__dirname, './public')));
+
+app.use(bodyParse.json());
+
+app.use('/api', api);
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
