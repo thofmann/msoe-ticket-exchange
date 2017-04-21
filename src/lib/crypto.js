a94b8d96fee669e0726704ea5bce147deb0a4655
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-20 21:06:00 -0500

    Config crypto

--- src/lib/crypto.js
+++ src/lib/crypto.js
@@ -1,18 +1,19 @@
 import crypto from 'crypto';
+import config from '../../config.json';
 
 export function createToken() {
     return crypto.randomBytes(32).toString('hex');
 }
 
 export function hash(message) {
-    return crypto.createHash('sha256').update(message).digest('hex');
+    return crypto.createHash(config.crypto.hash).update(message).digest('hex');
 }
 
 export function saltAndHash(password, salt) {
     return new Promise((resolve, reject) => {
         password = Buffer.from(password, 'hex');
         salt = Buffer.from(salt, 'hex');
-        crypto.pbkdf2(password, salt, 1e5, 512, 'sha256', (err, key) => {
+        crypto.pbkdf2(password, salt, config.crypto.iterations, 32, config.crypto.hash, (err, key) => {
             if (err) {
                 reject(err);
             } else {
