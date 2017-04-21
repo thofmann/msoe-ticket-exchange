26a1661e6e38d4c8763b13626778420ae77dde12
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-20 21:14:31 -0500

    Use configured port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import api from './api';
+import config from '../config.json';
 
 let app = express();
 
@@ -15,4 +16,4 @@ app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(8080, 'localhost');
+app.listen(config.server.port, 'localhost');
