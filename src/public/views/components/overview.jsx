85c8c122bdc30dfb29b887f131411eeed914fc20
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-24 14:37:54 -0500

    overview

--- src/public/views/components/overview.jsx
+++ src/public/views/components/overview.jsx
@@ -4,11 +4,15 @@ import { md5 } from '../../lib/crypto';
 export default class Overview extends React.Component {
 
     render() {
-        let src = `https://s.gravatar.com/avatar/${md5(this.props.backupEmail)}?s=150`;
-        src = `https://s.gravatar.com/avatar/${md5(this.props.studentEmail)}?s=150&d=${encodeURIComponent(src)}`;
+        let src = `https://s.gravatar.com/avatar/${md5(this.props.backupEmail)}?s=100`;
+        src = `https://s.gravatar.com/avatar/${md5(this.props.studentEmail)}?s=100&d=${encodeURIComponent(src)}`;
         return (
             <div className='overview pure-u-1-1 pure-u-md-1-2'>
                 <img src={src} />
+                <div className='details'>
+                    <span>Student email address: {this.props.studentEmail}</span>
+                    <span>Backup email address: {this.props.backupEmail}</span>
+                </div>
             </div>
         );
     }
