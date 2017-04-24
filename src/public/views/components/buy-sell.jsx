7cf1e0f64a14cb2e3510e6931338e060a4490700
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-24 15:42:19 -0500

    Switch between buy/sell

--- src/public/views/components/buy-sell.jsx
+++ src/public/views/components/buy-sell.jsx
@@ -9,14 +9,30 @@ export default class BuySell extends React.Component {
         };
     }
 
+    buySide() {
+        this.setState({
+            side: 'buy'
+        });
+    }
+
+    sellSide() {
+        this.setState({
+            side: 'sell'
+        });
+    }
+
     render() {
+        let buyClass = this.state.side === 'buy' ? 'buy active' : 'buy';
+        let sellClass = this.state.side === 'sell' ? 'sell active' : 'sell';
         return (
-            <div className='buy-sell-container pure-u-1-1 pure-u-md-1-2'>
+            <div className='buy-sell-container pure-u-1-1 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-1-4'>
                 <div className='buy-sell'>
-                    <span className='buy active'>Buy</span>
-                    <span className='sell'>Sell</span>
+                    <span className={buyClass} onClick={() => this.buySide()}>Buy</span>
+                    <span className={sellClass} onClick={() => this.sellSide()}>Sell</span>
                     <form>
+                        Quantity (tickets):
                         <input type='number' />
+                        Price (mBTC):
                         <input type='number' />
                         <input type='submit' />
                     </form>
--- src/public/views/components/overview.jsx
+++ src/public/views/components/overview.jsx
@@ -7,7 +7,7 @@ export default class Overview extends React.Component {
         let src = `https://s.gravatar.com/avatar/${md5(this.props.backupEmail)}?s=100`;
         src = `https://s.gravatar.com/avatar/${md5(this.props.studentEmail)}?s=100&d=${encodeURIComponent(src)}`;
         return (
-            <div className='overview-container pure-u-1-1 pure-u-md-1-2'>
+            <div className='overview-container pure-u-1-1 pure-u-sm-1-2 pure-u-md-2-3 pure-u-lg-3-4'>
                 <div className='overview'>
                     <img src={src} />
                     <div className='details'>
